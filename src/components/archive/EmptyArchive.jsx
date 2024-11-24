9a9a1ead1a6fb21b74f27a3e6b22f037b4705ba9
kummari-pavan <kpavan892002@gmail.com>
2024-11-24 14:25:27 +0530

    [PAVAN]ADDING: Archive and Trash pages

---
+++ src/components/archive/EmptyArchive.jsx
@@ -0,0 +1,29 @@
+import { Typography, Box, styled } from '@mui/material';
+
+
+const Text = styled(Typography)`
+    color: #80868b;
+    font-size: 22px
+`
+
+const Container = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20vh
+`
+
+const EmptyArchive = () => {
+    return (
+        <Container style={{marginLeft:"60px",marginTop:"100px" }}>
+             <img
+                src={`${process.env.PUBLIC_URL}/images/archive1.png`}
+                alt="Logo"
+                style={{ height: "130px", width: "auto"}}
+              />
+            <Text>Notes you Archived appear here</Text>
+        </Container>
+    )
+}
+
+export default EmptyArchive;
