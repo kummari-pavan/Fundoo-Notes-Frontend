1903408fdd6328d256e770bf8eb07e8955cec236
kummari-pavan <kpavan892002@gmail.com>
2024-11-24 14:01:06 +0530

    [PAVAN]ADDING:animation with js-confetti and ReactToastify

--- src/RouteModules.js
+++ src/RouteModules.js
@@ -2,9 +2,19 @@ import react from "react";
 import { createBrowserRouter,RouterProvider,useNavigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import NotesContainer from "./components/notes/NotesContainer"
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function RouteModules(){
     const routes=createBrowserRouter([
+        {
+            path: "login",
+            element: <Login/>
+        },
+        {
+            path: "register",
+            element: <Register/>
+        },
         {
             path: "dashboard",
             element: <Dashboard/>,
