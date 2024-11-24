c3bbb225a35af8e0754c5c968fb2459941c708ce
kummari-pavan <kpavan892002@gmail.com>
2024-11-24 13:55:21 +0530

    [PAVAN]ADDING:API calls for register and login from Backend

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,16 +1,5 @@
 import axios from "axios";
-// const BASE_URL=`http://localhost:4000/api/v1/`;
 
-
-
-
-// export const loginApiCall = async(payload,END_POINT) => {
-//     return await axios.post(`${BASE_URL}${END_POINT}`, payload)
-// }
-
-// export const signupApiCall = async(payload,END_POINT)=>{
-//     return await axios.post(`${BASE_URL}${END_POINT}`,payload)
-// }
 const API = axios.create({
     baseURL: "http://localhost:4000/api/v1",
   });
